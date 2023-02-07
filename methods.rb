2507c9a5b6578972b7d9be1de1cb0bdca5090f76
Student <venus.kabera@student.moringaschool.com>
2023-02-07 08:46:15 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+
+def greet(name = "Naureen!")
+    puts "Hello,#{name}!"
+end
+
+def greet(name = "Jimmy!")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="Naureen")
+    puts "Hello, #{name}!"
+end
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+def add(num1,num2)
+    return num1 + num2  
+end
+def halve(number=6)
+    if number.class != Integer
+         return nil
+    end
+    number / 2
+end
